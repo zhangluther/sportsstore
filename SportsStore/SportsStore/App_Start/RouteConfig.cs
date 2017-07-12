ccbcd985ff1f39daf8d12948751618e46d8d5e93
Zhang Wei <zhangluther@yahoo.com>
2017-07-12 11:47:54 +0800

    0.1

---
+++ SportsStore/SportsStore/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace SportsStore.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute(null, "", "~/Pages/Listing.aspx");
+            routes.MapPageRoute(null, "list/{category}/{page}", "~/Pages/Listing.aspx");
+            routes.MapPageRoute(null, "list/{page}", "~/Pages/Listing.aspx");
+            routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
+        }
+    }
+}
